de8f06e28fc55b96637506d6a7859d1fc2e16758
DevGupta <devguptamsi@gmail.com>
2026-02-10 19:22:34 +0530

    Added CustomUserDetailsService

--- src/main/java/com/underground/fitness/security/CustomUserDetailsService.java
+++ src/main/java/com/underground/fitness/security/CustomUserDetailsService.java
@@ -1,5 +1,32 @@
 package com.underground.fitness.security;
 
-public class CustomUserDetailsService {
+import com.underground.fitness.models.User;
+import com.underground.fitness.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService  implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
+        if(user == null){
+            throw new UsernameNotFoundException(email);
+        }
+
+        return org.springframework.security.core.userdetails.User.builder().
+                username(user.getEmail())
+                .password(user.getPassword())
+                .roles(user.getRole().name())
+                .build();
+    }
 }
