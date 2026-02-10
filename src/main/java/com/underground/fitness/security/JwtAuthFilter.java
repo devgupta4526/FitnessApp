498a4839381202ccd32eddc6c3d22e7dcfa0d53a
DevGupta <devguptamsi@gmail.com>
2026-02-10 19:09:12 +0530

    Added the Jwt Authentication Filter

--- src/main/java/com/underground/fitness/security/JwtAuthFilter.java
+++ src/main/java/com/underground/fitness/security/JwtAuthFilter.java
@@ -1,4 +1,65 @@
 package com.underground.fitness.security;
 
-public class JwtAuthFilter {
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+
+@Component
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JwtUtils jwtUtils;
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+        try{
+            String jwt = parseJwt(request);
+
+            if(jwt != null && jwtUtils.validateJwtToken(jwt)){
+                String userId = jwtUtils.getUserIdFromToken(jwt);
+                Claims claims = jwtUtils.getClaimsFromToken(jwt);
+                List<String> roles = claims.get("role", List.class);
+                List<GrantedAuthority> grantedAuthorities = List.of();
+                if(roles != null){
+                    grantedAuthorities = roles.stream().map(role ->(GrantedAuthority)
+                            new SimpleGrantedAuthority(role)).toList();
+                }
+
+                UsernamePasswordAuthenticationToken authentication =
+                        new UsernamePasswordAuthenticationToken(userId,
+                                null,
+                                grantedAuthorities);
+
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        filterChain.doFilter(request,response);
+    }
+
+    private String parseJwt(HttpServletRequest request){
+        String jwt = jwtUtils.getJwtFromHeader(request);
+        return jwt;
+    }
 }
--- src/main/java/com/underground/fitness/security/SecurityConfig.java
+++ src/main/java/com/underground/fitness/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.underground.fitness.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,14 +8,14 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
-
-
+    private final JwtAuthFilter jwtAuthFilter;
     public SecurityFilterChain securityFilterChain(HttpSecurity http) {
-
         http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests.requestMatchers("/api/v1/admin/**").hasRole("ADMIN")
@@ -22,6 +23,7 @@ public class SecurityConfig {
                                 .anyRequest().authenticated()
 
                 );
+        http.addFilter(jwtAuthFilter);
         return http.build();
     }
 
